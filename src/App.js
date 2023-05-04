133b77e3b9ddd99022a4057f54ed7a6d228a9134
PMccormick1 <51823622+Pmac101@users.noreply.github.com>
2023-05-03 21:13:59 -0400

    added missing files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import React, { Component, useState, useEffect } from "react";
+import Filters from "./components/filters/Filters";
+import Cards from "./components/cards/Cards";
 
+
+// main program component
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    // sets default page number. This only works because the api has pages...needs modification for other api
+    let [pageNumber, setPageNumber] = useState(1);
+    let [fetchData, updateFetchData] = useState([]);
+    let {info, results} = fetchData;
+    let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+    // whenever app page changes, this will fetch the new data
+    useEffect(() => {
+        (async function () {
+            // wait for data to be fetched form API
+            let data = await fetch(api).then(res => res.json());
+            updateFetchData(data);
+        })()
+    }, [api])
+
+    return (
+        <div className='App'>
+            <h1 className='text-center ubuntu my-4'>Loot Database</h1>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col-3'>
+                        <Filters/>
+                    </div>
+                    <div className='col-8'>
+                        <div className='row'>
+                            {/* passes results to Card component */}
+                            <Cards results={results} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App;
